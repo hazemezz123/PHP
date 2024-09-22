fe5bb76d21d0848eb2dfa6f647fa2e02778393be
hazemezz123 <hazemezz988@gmail.com>
2024-09-22 23:49:24 +0300

    php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+// single line comment 
+/* multiple Line Comment */
+
+// Print Section
+echo "hell world";
+echo"<h1> hello world </h1>";
+echo"<div><h1> hello world <span> Bogs? </span> <br> End?</h1></div>";
+print "the hide side of the future and the make of ";
+// end of print section 
