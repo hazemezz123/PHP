15ac9c8a15de74ae03821008e9a4b8c507da25c5
hazemezz123 <hazemezz988@gmail.com>
2024-09-23 01:05:31 +0300

    --

--- index.php
+++ index.php
@@ -8,3 +8,12 @@ echo"<h1> hello world </h1>";
 echo"<div><h1> hello world <span> Bogs? </span> <br> End?</h1></div>";
 print "the hide side of the future and the make of ";
 // end of print section 
+
+echo "<hr>";
+
+// Start of DataType
+echo gettype(true);
+echo "<br>";
+echo gettype("hello world");
+echo "<br>";
+echo gettype(1000);
