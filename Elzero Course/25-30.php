81e38eaf17641e08f416e13a128645343c86faa0
hazemezz123 <hazemezz988@gmail.com>
2024-10-04 20:44:11 +0300

    ...

--- Elzero Course/25-30.php
+++ Elzero Course/25-30.php
@@ -1,4 +1,25 @@
 <?php
 
-$dolorSign = "tha main person in my life is not reel !?";
-echo "" . $dolorSign . "";
+// Logical Operators -- 
+
+var_dump(100 > 60 and 100 > 40 and 100 > 50);
+echo "<br>";
+var_dump(100 > 60 and 100 > 40 and 100 > 50 and 100 > 120);
+echo "<br>";
+var_dump(100 > 60 and 100 > 40 and 100 > 50 or 100 > 120);
+echo "<br>";
+var_dump(100 > 120 or 100 > 100 or 100 > 140 or 100 > 90);
+echo "<br>";
+var_dump(100 > 120 xor 100 > 120);
+echo "<br>";
+var_dump(100 > 120 xor 100 > 90);
+echo "<br>";
+// String Operators 
+
+$lines = 5;
+for ($i = 1; $i <= $lines; $i++) {
+    for ($j = 1; $j <= $i; $j++) {
+        echo "*";
+    }
+    echo "<br>";
+}
